d84afdaa607bbf16fa3b9b18205bdb2d065b41b2
Samartha Bhat <samarthabhat11@gmail.com>
2025-09-20 21:29:52 +0530

    RESTful

--- Building Cloud Native Service/Simple API/go.mod
+++ Building Cloud Native Service/Simple API/go.mod
@@ -1,3 +1,5 @@
 module github.com/Samarthasbhat/Cloud-Native-Go/Building-Cloud-Native-Service/Simple-API
 
 go 1.24.1
+
+require github.com/gorilla/mux v1.8.1
--- Building Cloud Native Service/Simple API/main.go
+++ Building Cloud Native Service/Simple API/main.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"errors"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 // This is creating the core component of the API Services with GET<PUT<DELETE functions
@@ -28,5 +33,38 @@ func Delete(key string) error{
 	delete(store, key)   //Inbuilt function to delete a key from a map
 	return nil
 }
+
+func KeyValuePutHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+
+	value, err := io.ReadAll(r.Body)
+	defer r.Body.Close()
+
+	if err != nil {
+		http.Error(w, 
+			err.Error(),
+			http.StatusInternalServerError)
+		return
+	}
+	err = Put(key, string(value))
+	if err != nil {
+		http.Error(w, 
+			err.Error(),
+			http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+}
+
+
+func main(){
+r := mux.NewRouter()
+
+	r.HandleFunc("/v1/keys/{key}", KeyValuePutHandler).Methods("PUT")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
 
 
